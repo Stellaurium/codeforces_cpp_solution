a3f39800955997f30d2d30f71ac38b652a241197
stellaura <stellaura@outlook.com>
2024-09-07 14:31:12 +0800

    cf_971_div4 E binary search

--- contest/cf_971_div4/E/E.cpp
+++ contest/cf_971_div4/E/E.cpp
@@ -4,6 +4,8 @@
 #include "BigInt.hpp"
 #include <bits/stdc++.h>
 
+#pragma GCC optimize("O3")
+
 using namespace std;
 
 // 这部分的精度足够了 n k i 都是 int类型内
@@ -11,10 +13,13 @@ using namespace std;
 // int64 是 2^63 大约为 9e18
 int64_t value(int64_t n, int64_t k, int64_t i) {
     // 2 i + 2 i^2 + 2 k - 2 k^2 + n - 2 k n - n^2
-    return abs(2 * i + 2 * i * i + 2 * k - 2 * k * k + n - 2 * k * n - n * n);
+    return (2 * i + 2 * i * i + 2 * k - 2 * k * k + n - 2 * k * n - n * n)/2;
 }
 
-int64_t calc(int64_t n, int64_t k) {
+
+// 1.5s 这个太慢了 再找找别的高精度库
+// 374 ms 效果好多了
+int64_t calc_with_bigint(int64_t n, int64_t k) {
     // i -> 1/2 (-1 + Sqrt[1 - 4 k + 4 k^2 - 2 n + 4 k n + 2 n^2])
     BigInt big_n{n};
     BigInt big_k{k};
@@ -24,20 +29,43 @@ int64_t calc(int64_t n, int64_t k) {
 
     int64_t before = x.to_long_long();
     int64_t after = before + 1;
-    int64_t before_value = value(n, k, before);
-    int64_t after_value = value(n, k, after);
-    return min(before_value, after_value) / 2;
+    int64_t before_value = abs(value(n, k, before));
+    int64_t after_value = abs(value(n, k, after));
+    return min(before_value, after_value) ;
 }
 
-// 1.5s 这个太慢了 再找找别的高精度库
-// 374 ms 效果好多了
+// 目的是找到第一个满足 >= 0 的位置
+int64_t binary_search_recursive(int64_t n, int64_t k, int64_t left, int64_t right) {
+    if (left == right) {
+        return left;
+    }
+    auto mid = (left + right) / 2;
+    int64_t mid_value = value(n, k, mid);
+    if (mid_value >= 0) {
+        // mid_value 当前位置可能就满足
+        return binary_search_recursive(n, k, left, mid);
+    } else {
+        // 当前位置<0 至少应该向右一格
+        return binary_search_recursive(n, k, mid + 1, right);
+    }
+}
+
+// 62ms 这东西比O(1)的算法还快
+int64_t binary_search(int64_t n, int64_t k) {
+    auto index = binary_search_recursive(n, k, k - 1, k + n - 1);
+    return min(abs(value(n, k, index)), abs(value(n, k, index - 1)));
+}
+
 int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
     int t;
     int64_t n, k;
     cin >> t;
     while (t--) {
         cin >> n >> k;
-        cout << calc(n, k) << endl;
+        cout << binary_search(n, k) << endl;
     }
     return 0;
 }
---
+++ contest/cf_971_div4/F/F.cpp
@@ -0,0 +1,3 @@
+//
+// Created by stellaura on 07/09/24.
+//
